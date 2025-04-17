f26673051b5b4bd2a37547b53a8f007f659dc473
fox-hedgehog <84984730+fox-hedgehog@users.noreply.github.com>
2025-04-17 16:18:53 +0800

    string4

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,21 +3,43 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+// I AM DONE
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    let mut l = 0;
+    let mut r = input.len();
+    for ch in input.chars() {
+        if ch == ' ' {
+            l += 1;
+        } else {
+            break;
+        }
+    }
+
+    for ch in input.chars().rev() {
+        if ch == ' ' {
+            r -= 1;
+        } else {
+            break;
+        }
+    }
+
+    if l >= r {
+        return String::new();
+    }
+
+    input[l..r].to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    input.to_string() + &" world!".to_string()
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    input.replace("cars", "balloons")
 }
 
 #[cfg(test)]
