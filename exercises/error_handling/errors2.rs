f855e47138e99063e00a84889de20256286b1db9
fox-hedgehog <84984730+fox-hedgehog@users.noreply.github.com>
2025-04-18 17:10:32 +0800

    50

--- exercises/error_handling/errors2.rs
+++ exercises/error_handling/errors2.rs
@@ -19,14 +19,22 @@
 // Execute `rustlings hint errors2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+// I AM DONE
 
-use std::num::ParseIntError;
+pub use std::num::ParseIntError;
 
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let processing_fee = 1;
     let cost_per_item = 5;
-    let qty = item_quantity.parse::<i32>();
+    let qty = match item_quantity.parse::<i32>() {
+        Ok(q) => q,
+        Err(e) => return Err(e),
+    };
+
+    // let qty = match qty {
+    //     Ok(q) => q,
+    //     Err(e) => Err(e),
+    // };
 
     Ok(qty * cost_per_item + processing_fee)
 }
